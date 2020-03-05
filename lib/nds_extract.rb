9f3712210f12293b1172da0388d6619cf854ffa8
Zhiyuan Liu <cloudnotes.64@gmail.com>
2020-03-05 14:43:59 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 
 def directors_totals(source)
+  a
   result = {}
   director_index = 0
   while director_index < source.size do
